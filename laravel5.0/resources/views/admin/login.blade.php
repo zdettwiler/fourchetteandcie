47faf9f8be9496237aecc140e4cd81da1d254c61
z.dettwiler <z.dettwiler@gmail.com>
2015-08-22 09:50:33 +0200

    center admin login form

--- laravel5.0/resources/views/admin/login.blade.php
+++ laravel5.0/resources/views/admin/login.blade.php
@@ -8,7 +8,7 @@
 	<link rel="stylesheet" href="http://www.fourchetteandcie.com/css/nav.css" type="text/css" media="all"/>
 	<link rel="stylesheet" href="http://www.fourchetteandcie.com/css/basket.css" type="text/css" media="all"/>
 	<link rel="stylesheet" href="http://www.fourchetteandcie.com/css/admin_forms.css" type="text/css" media="all"/>
-	
+
 	<script src="http://code.jquery.com/jquery.js"></script>
 	<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
 	<script src="http://www.fourchetteandcie.com/js/layout.js"></script>
@@ -25,21 +25,22 @@
 			</p>
 		</div>
 	@endif
-	
+
 @stop
 
 @section('content')
+	<div style="margin: auto; width: 215px; margin-top: 20%;">
+		<h2>Please Log In</h2>
 
-	<h2>Please Log In</h2>
+		{{-- @foreach ($users as $user)
+			{{ $user['id'] }} {{ $user['username'] }} {{ $user['password'] }}{!! '<br>' !!}
+		@endforeach --}}
 
-	{{-- @foreach ($users as $user)
-		{{ $user['id'] }} {{ $user['username'] }} {{ $user['password'] }}{!! '<br>' !!}
-	@endforeach --}}
+		{!! Form::open() !!}
+			{!! Form::text('username', 'username') !!}<br>
+			{!! Form::password('password') !!}<br><br>
 
-	{!! Form::open() !!}
-		{!! Form::text('username', 'username') !!}<br>	
-		{!! Form::password('password') !!}<br><br>
-
-		{!! Form::submit('LOGIN'); !!}
-	{!! Form::close() !!}
+			{!! Form::submit('LOGIN'); !!}
+		{!! Form::close() !!}
+	</div>
 @stop
