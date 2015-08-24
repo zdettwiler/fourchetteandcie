50f89aff87a6b08c76b5220d4afaa13868e43866
z.dettwiler <z.dettwiler@gmail.com>
2015-08-24 12:03:28 +0200

    OrderValidation fix for order currency

--- laravel5.0/app/Facades/OrderValidation.php
+++ laravel5.0/app/Facades/OrderValidation.php
@@ -35,7 +35,7 @@ class OrderValidation
 
 			$VALIDATION_ORDER = array_merge($VALIDATION_ORDER, $order_details);
 
-			dd($VALIDATION_ORDER);
+			// dd($VALIDATION_ORDER);
 			// Add the 'comment' for each item
 			for($i=0 ; $i<=count($VALIDATION_ORDER)-11 ; $i++)
 			{
