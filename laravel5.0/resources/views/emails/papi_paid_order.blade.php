c39f1eee0a17f52de84469bd920c77aaf91dcac1
z.dettwiler <z.dettwiler@gmail.com>
2015-08-29 22:27:11 +0200

    papi_paid_order order_id fix

--- laravel5.0/resources/views/emails/papi_paid_order.blade.php
+++ laravel5.0/resources/views/emails/papi_paid_order.blade.php
@@ -14,7 +14,7 @@
 		The order nº{{ sprintf('%03u', $order_id) }} has been paid, and the invoice is finalised. You can check it out by following the link below.
 		<br>Keep up the good work!</p>
 
-	<a href="http://www.fourchetteandcie.com/admin/orders/{{ $order->id }}" style="display: block; margin: auto; width: 70%; color: #FFFFFF; text-align: center; font-size: 18px; font-family: Arial, sans-serif; text-decoration: none; padding: 15px 0; background-color: #7cffb1; border-radius: 7px;">VIEW THE ORDER</a>
+	<a href="http://www.fourchetteandcie.com/admin/orders/{{ $order_id }}" style="display: block; margin: auto; width: 70%; color: #FFFFFF; text-align: center; font-size: 18px; font-family: Arial, sans-serif; text-decoration: none; padding: 15px 0; background-color: #7cffb1; border-radius: 7px;">VIEW THE ORDER</a>
 
 	<p style="font-family: sans-serif; font-size: 16px; font-weight: 100; padding: 0 20px;">With love,<br>Zach</p>
 
