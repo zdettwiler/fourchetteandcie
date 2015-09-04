372169f0fbb3ef5d40b9ce5935e066c9a362355f
z.dettwiler <z.dettwiler@gmail.com>
2015-09-04 11:56:33 +0200

    shorter shopwindow for css full load on mobile test

--- laravel5.0/app/Http/Controllers/CutleryController.php
+++ laravel5.0/app/Http/Controllers/CutleryController.php
@@ -17,7 +17,7 @@ class CutleryController extends Controller
 	{
 		$items = DB::table('cutlery')
 					->orderBy('is_new', 'desc')
-					// ->take(10)
+					->take(10)
 					->get();
 
 		return view('cutlery.index', compact('items'));
