0ad497e87fdd3e7d5a0a8d51691ed494c630bf0d
z.dettwiler <z.dettwiler@gmail.com>
2015-09-05 19:28:48 +0200

    got rid of useless commented old order validation

--- laravel5.0/app/Facades/OrderValidation.php
+++ laravel5.0/app/Facades/OrderValidation.php
@@ -319,7 +319,7 @@ class OrderValidation
 	}
 
 //----------------------------------------------------------------------------//
-// MAKE THE HTML TABLE CONTAINING THE ORDER AND DETAILS
+// SEND BACK THE JSON ORDER
 //----------------------------------------------------------------------------//
 	public static function validation_table_html($order_id)
 	{
@@ -327,108 +327,6 @@ class OrderValidation
 
 		echo json_encode($ORDER, JSON_PRETTY_PRINT);
 		return false;
-		/*$VALIDATION_ORDER = Session::get('validation_order', []);
-		$section_ref_code = Config::get('fandc_arrays')['section_ref_code'];
-
-		if($VALIDATION_ORDER['currency'] == 'eur')
-			$currency = '&euro;';
-		elseif($VALIDATION_ORDER['currency'] == 'aud')
-			$currency = 'AU$';
-
-		// is_wholesale AND TABLE HEADER
-		$response = "{$VALIDATION_ORDER['is_wholesale']}{$VALIDATION_ORDER['currency']}<tr class='table-header'>
-			<td></td>
-			<td style='width:50%;'>Item Description</td>
-
-			<td>Qty</td>
-			<td>Unit. Price</td>
-			<td>Total</td>
-		</tr>\n";
-
-		// ITEMS
-		for($i=0 ; $i<=count($VALIDATION_ORDER)-11 ; $i++)
-		{
-			if($VALIDATION_ORDER[$i]['qty'] > 1)
-			{
-				$minus_button = '-';
-			}
-			else
-			{
-				$minus_button = "<img src='http://www.fourchetteandcie.com/pictures/bin.png' height='20'>";
-			}
-
-			$response .= "<tr item-ref='{$VALIDATION_ORDER[$i]['ref']}'>\n
-				<td><img class='item-img' src='";
-
-			if(substr($VALIDATION_ORDER[$i]['ref'], 0, 2) != "cu")
-			{
-				$response .= "http://www.fourchetteandcie.com/pictures/{$section_ref_code[ $VALIDATION_ORDER[$i]['ref'][0] ]}/100px/{$VALIDATION_ORDER[$i]['ref']}_thumb.jpg";
-			}
-
-			$response .= "' height='50'></td>\n
-
-				<td style='width: 50%;'><span class='ref-box'>{$VALIDATION_ORDER[$i]['ref']}</span> {$VALIDATION_ORDER[$i]['name']} <i>{$VALIDATION_ORDER[$i]['descr']}</i><br><input class='edit-comment' type='text' name='comment-{$VALIDATION_ORDER[$i]['ref']}' placeholder='comment' value='{$VALIDATION_ORDER[$i]['comment']}'></td>\n
-
-				<td><div class='item-qty'><div class='item-qty-plus-button'>+</div> <div class='item-qty-value'>{$VALIDATION_ORDER[$i]['qty']}</div> <div class='item-qty-minus-button'>{$minus_button}</div></div></td>\n
-
-				<td class='edit-field'>". $currency ."<input class='edit-unit-price' type='text' name='unit-price-{$VALIDATION_ORDER[$i]['ref']}' value='".number_format($VALIDATION_ORDER[$i]['price'] , 2)."'></td>\n
-
-				<td>". $currency ."".number_format($VALIDATION_ORDER[$i]['qty'] * $VALIDATION_ORDER[$i]['price'], 2)."</td>\n
-
-			</tr>";
-		}
-
-		// SEARCH ENGINE
-		$response .= "	<tr id='search-add-item'>\n
-				<td colspan='5'>
-					<p id='search-info'>try <i>+{space}</i>, <i>#ref</i>, <i>&#36;section</i> or <i>@category</i>.</p>
-					<div id='search-container'>
-						<div id='search-box'>
-							<div id='search-tags'></div>
-							<div style='overflow: hidden'>
-								<input id='search-input' type='text' autocomplete='off' placeholder='search an item' >
-							</div>
-						</div>
-						<div id='results-box'><table> </table></div>
-					</div>
-				</td>
-			</tr>
-
-			<tr id='subtotal-row'>\n
-				<td colspan='4'>SUBTOTAL ({$VALIDATION_ORDER['nb_items']} item";
-		// SUBTOTAL etc.
-		if($VALIDATION_ORDER['nb_items'] > 1)
-		{
-			$response .= "s";
-		}
-
-		$response .= ")</td>\n
-				<td>". $currency ."".number_format($VALIDATION_ORDER['subtotal'], 2)."</td>\n
-			</tr>\n";
-
-		if($VALIDATION_ORDER['is_wholesale'])
-		{
-			$response .= "	<tr id='subtotal-row'>\n
-				<td colspan='4'>WHOLESALE (-30%)</td>\n
-				<td>". $currency ."".number_format( 0.7 * $VALIDATION_ORDER['subtotal'], 2 )."</td>\n
-			</tr>\n";
-		}
-
-		$response .= "	<tr id='shipping-row'>\n
-				<td colspan='4'>SHIPPING <input class='edit-shipping-details' type='text' name='shipping-details' placeholder='shipping details' value='{$VALIDATION_ORDER['shipping_details']}'></td>\n
-				<td class='edit-field'>". $currency ."<input class='edit-shipping' type='text' name='shipping' value='".number_format($VALIDATION_ORDER['shipping'], 2)."'></td>\n
-			</tr>
-
-			<tr id='total-row'>\n
-				<td colspan='4'>TOTAL</td>\n
-				<td>". $currency ."".number_format( $VALIDATION_ORDER['total'], 2 )."</td>\n
-			</tr>
-
-			<tr id='message-row'>\n
-				<td colspan='5'>Add a message:<br><textarea>{$VALIDATION_ORDER['message']}</textarea></td>\n
-			</tr>";
-
-		echo $response;*/
 	}
 
 //----------------------------------------------------------------------------//
