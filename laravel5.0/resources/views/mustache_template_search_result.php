be0bd2014ca7a82821a19b6fbe1b948739801dd4
z.dettwiler <z.dettwiler@gmail.com>
2015-09-21 15:49:44 +0200

    mustache template interprets html char

--- laravel5.0/resources/views/mustache_template_search_result.php
+++ laravel5.0/resources/views/mustache_template_search_result.php
@@ -2,8 +2,8 @@
     <tr class='result' item-ref='{{ref}}'>
         <td class='result-img'><img src="http://fourchetteandcie.com/pictures/{{ref_section}}/500px/{{ref}}.jpg" width="200px" height="200px"></td>
         <td class='result-details'>
-            <p><span class='ref-box'>{{ref}}</span> {{name}}<br>
-                <i>{{descr}}</i> ({{categ}})</p>
+            <p><span class='ref-box'>{{ref}}</span> {{{name}}}<br>
+                <i>{{{descr}}}</i> ({{categ}})</p>
         </td>
         <td class='result-price'>
             <p>€ {{price}}</p>
